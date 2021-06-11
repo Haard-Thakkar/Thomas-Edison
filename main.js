be0a886e233e7d58d201f3df9c24b10e815cd43e
Haard-Thakkar <85060037+Haard-Thakkar@users.noreply.github.com>
2021-06-11 18:16:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Turnon() {
+document.getElementById("Bulb").src="https://s3-projectsassets.whjr.online/On+Bulb.png"
+}
+
+function Turnoff() {
+document.getElementById("Bulb").src="https://s3-projectsassets.whjr.online/Off+Bulb.png"
+}
